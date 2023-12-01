77eb09fb8685894477ff9c9074b1e4d136f0c7f0
Igor Liakh <igorlyah060@gmail.com>
2023-12-01 22:01:23 +0200

    added back link to page 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
-  return <div>404! Page not found!</div>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <h2
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        404! Page not found!
+      </h2>
+    </div>
+  );
 };
 
 export default NotFoundPage;
